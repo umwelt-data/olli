34d2da24a14ed04dae1e1678cca2e475212def62
Jonathan Zong <jonathan@jonathanzong.com>
2026-05-15 16:46:09 -0600

    fix build

--- apps/docs/.vitepress/config.ts
+++ apps/docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   vite: {
-    plugins: [solidPlugin()],
+    plugins: [solidPlugin({ ssr: true })],
     resolve: {
       conditions: ['development', 'browser'],
     },
